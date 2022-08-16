a87946c5ef4153a531fe180cf9c46d8f5f37fb2e
swatibongu <78065014+swatibongu@users.noreply.github.com>
2022-08-16 20:49:39 +0530

    Welcome to employee wage.

--- employwage.sh
+++ employwage.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x
 
 echo "Welcome to employee wage program."
+ispresent=1
+randomCheck=$((RANDOM%2))
+
+if [ $ispresent -eq $randomCheck ]
+then
+echo "Employee is present."
+else
+echo "Employee is absent."
+fi
